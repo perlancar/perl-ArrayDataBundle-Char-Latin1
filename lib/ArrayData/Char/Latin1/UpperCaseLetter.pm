451e8b7f9464eb3231b6710e09fe8f7dfbcda0ea
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-20 19:37:44 +0700

    Add module ArrayData::Char::Latin1::UpperCaseLetter

---
+++ lib/ArrayData/Char/Latin1/UpperCaseLetter.pm
@@ -0,0 +1,52 @@
+package ArrayData::Char::Latin1::UpperCaseLetter;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use Role::Tiny::With;
+with 'ArrayDataRole::Source::LinesInDATA';
+# minimalistic object, no additional roles loaded by default
+
+# STATS
+
+1;
+# ABSTRACT: Latin1 uppercase letters
+
+=head1 DESCRIPTION
+
+For testing only.
+
+
+=head1 SEE ALSO
+
+=cut
+
+__DATA__
+A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z
